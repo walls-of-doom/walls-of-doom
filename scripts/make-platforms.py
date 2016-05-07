3bab78cb31b7791880dc976e560570b5243243d1
Bernardo Sulzbach <mafagafogigante@gmail.com>
2016-05-07 18:24:54 -0300

    make-platforms.py should write the platform count

--- scripts/make-platforms.py
+++ scripts/make-platforms.py
@@ -1,7 +1,9 @@
-import random
+from random import randint
 
 
 if __name__ == '__main__':
     with open('platforms.txt', 'w') as output:
-        for i in range(16):
-            print('{} {}'.format(random.randint(5, 15), random.randint(1, 3)), file=output)
+        platform_count = 16
+        print('{}'.format(platform_count), file=output)
+        for i in range(platform_count):
+            print('{} {}'.format(randint(5, 15), randint(1, 3)), file=output)
