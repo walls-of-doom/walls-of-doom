f33f0ed4f810814aae19c885c08d986ce843153f
Bernardo Sulzbach <mafagafogigante@gmail.com>
2016-05-23 19:31:42 -0300

    Packaging script should have comments
    Also neatly producing a PDF version of the README file.
    
    Related to #107

--- package.sh
+++ package.sh
@@ -3,13 +3,25 @@
 NAME="sulzbach-ferrazza.zip"
 HELPER="packaging-helper"
 
+# Delete the old file if it exists
 rm -f $NAME
+
+# Zip the current tree, except for .git, blender, and build
 zip -q -r $NAME . -x .\* blender/\* build/\*
 
+# Create the helper directory
 mkdir $HELPER
 cd $HELPER
+# Build everything in the helper directory
 cmake ..
 make
 cd ..
+# Add the executable to the archive
 zip -q -j $NAME $HELPER/game/walls-of-doom
+# Clean the helper directory up
 rm -rf $HELPER
+
+# Create a PDF from the README using Pandoc
+pandoc -t latex README.md -o README.pdf
+zip -q $NAME README.pdf
+rm README.pdf
