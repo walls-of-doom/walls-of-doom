e33c95a2caf2c11d8a8fccff6562aa67ece9b918
Bernardo Sulzbach <mafagafogigante@gmail.com>
2016-10-08 02:39:55 -0300

    Improvements and fixes for the release script
    Should not try to modify the POM.
    Should also push the tag to upstream.

--- release.sh
+++ release.sh
@@ -3,15 +3,8 @@ if [ $# -ne 2 ]
         echo "Pass two arguments: the new tag and a short release message."
     else
         old_tag=$(git describe --abbrev=0)
-        sed -i "s/$old_tag/$1/g" pom.xml
         git commit -a -m "$(printf "Release $1\n\n$2")"
         git tag -a $1 -m "$2"
 	git push origin HEAD --follow-tags
-	# git push origin --set-upstream HEAD
-	#   A handy way to push the current branch to the same name on the
-	#   remote.
-	# --follow-tags
-	#   Also push annotated tags in refs/tags that are missing from the
-	#   remote but are pointing at commit-ish that are reachable from the
-	#   refs being pushed
+	git push upstream HEAD --follow-tags
 fi
