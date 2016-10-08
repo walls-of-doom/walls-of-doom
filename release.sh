161d44e8ff568e711dcd0092ccb871aa348847e3
Bernardo Sulzbach <mafagafogigante@gmail.com>
2016-10-08 02:57:08 -0300

    Should freeze version in CMakeLists on releases

--- release.sh
+++ release.sh
@@ -1,10 +1,11 @@
 if [ $# -ne 2 ]
-    then
-        echo "Pass two arguments: the new tag and a short release message."
-    else
-        old_tag=$(git describe --abbrev=0)
-        git commit -a -m "$(printf "Release $1\n\n$2")"
-        git tag -a $1 -m "$2"
-	git push origin HEAD --follow-tags
-	git push upstream HEAD --follow-tags
+then
+    echo "Pass two arguments: the new tag and a short release message."
+else
+    old_tag=$(git describe --abbrev=0)
+    sed -i "s/$old_tag/$1/g" CMakeLists.txt
+    git commit -a -m "$(printf "Release $1\n\n$2")"
+    git tag -a $1 -m "$2"
+    git push origin HEAD --follow-tags
+    git push upstream HEAD --follow-tags
 fi
