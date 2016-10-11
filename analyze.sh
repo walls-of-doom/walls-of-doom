e8cd8b84895fd619abf534c1545a29110a28732b
Bernardo Sulzbach <mafagafogigante@gmail.com>
2016-10-11 08:04:10 -0300

    Should have two different reformatting scripts
    Clang-tidy does not need to be run every time reformatting happens.
    
    Closes #77

--- analyze.sh
+++ analyze.sh
@@ -5,8 +5,8 @@ if [ ! -f $COMPILATION_DATABASE ]; then
   ln -s build/compile_commands.json
 fi
 
-echo Reformatting...
-clang-format -i walls-of-doom/*.[hc]
-clang-format -i tests/*.[hc]
+echo Analyzing...
+clang-tidy walls-of-doom/*.[c]
+clang-tidy tests/*.[c]
 
 echo "Done after $SECONDS seconds."
