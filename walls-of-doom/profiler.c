9eade9e8f6a1678dae7c5b0581b1e05462c7a281
Bernardo Sulzbach <mafagafogigante@gmail.com>
2016-10-12 18:09:35 -0300

    Should take identifier into profiler entries order
    This commit introduces changes that make the profiler group output based
    on the identifier up to the first occurrence of the colon.
    
    Closes #86

--- walls-of-doom/profiler.c
+++ walls-of-doom/profiler.c
@@ -1,9 +1,11 @@
+#include "profiler.h"
+
 #include "clock.h"
-#include "code.h"
 #include "constants.h"
 #include "data.h"
 #include "logger.h"
 #include "memory.h"
+#include "sort.h"
 #include "text.h"
 
 #include <stdio.h>
@@ -79,9 +81,29 @@ static double profiler_data_mean(const ProfilerData *const data) {
   return data->sum / (double)data->frequency;
 }
 
+static void profiler_data_copy_base_id(const ProfilerData *data, char *dest) {
+  char *colon = strchr(data->identifier, ':');
+  copy_string(dest, data->identifier, MAXIMUM_DATA_IDENTIFIER_SIZE);
+  if (colon != NULL) {
+    /* Cut the string before the colon. */
+    dest[colon - data->identifier] = '\0';
+  }
+}
+
 static int profiler_data_greater_than(const void *a, const void *b) {
-  const double a_mean = profiler_data_mean((ProfilerData *)a);
-  const double b_mean = profiler_data_mean((ProfilerData *)b);
+  const ProfilerData *a_data = (ProfilerData *)a;
+  const ProfilerData *b_data = (ProfilerData *)b;
+  const double a_mean = profiler_data_mean(a_data);
+  const double b_mean = profiler_data_mean(b_data);
+  char a_base_id[MAXIMUM_DATA_IDENTIFIER_SIZE];
+  char b_base_id[MAXIMUM_DATA_IDENTIFIER_SIZE];
+  int comparison;
+  profiler_data_copy_base_id(a_data, a_base_id);
+  profiler_data_copy_base_id(b_data, b_base_id);
+  comparison = strcmp(a_base_id, b_base_id);
+  if (comparison != 0) {
+    return comparison;
+  }
   if (a_mean < b_mean) {
     return 1;
   }
@@ -92,7 +114,7 @@ static int profiler_data_greater_than(const void *a, const void *b) {
 }
 
 void sort_table(void) {
-  qsort(table, table_size, sizeof(ProfilerData), profiler_data_greater_than);
+  sort(table, table_size, sizeof(ProfilerData), profiler_data_greater_than);
 }
 
 void write_statistics(void) {
