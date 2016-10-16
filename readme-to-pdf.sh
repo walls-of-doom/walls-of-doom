58cad594458601397c8c70110b511ea606c9cac3
Bernardo Sulzbach <mafagafogigante@gmail.com>
2016-10-16 07:17:15 -0200

    Removed useless documentation-generating files

--- readme-to-pdf.sh
+++
@@ -1,6 +0,0 @@
-pandoc -N --template=template.tex\
-    --variable mainfont="Liberation Sans"\
-    --variable sansfont="Liberation Sans"\
-    --variable monofont="Liberation Mono"\
-    --variable fontsize=12pt\
-    README.md --latex-engine=xelatex --toc -o README.pdf
