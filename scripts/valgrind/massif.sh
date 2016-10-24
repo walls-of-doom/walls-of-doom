2af6115e92c3661e1bd92235c3df9c929f216931
Bernardo Sulzbach <mafagafogigante@gmail.com>
2016-10-24 07:25:01 -0200

    The Valgrind scripts should change directory
    They can no longer execute the game as they used to do.

--- scripts/valgrind/massif.sh
+++ scripts/valgrind/massif.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 #
 # This is a single-command script to run Massif, a heap profiler.
-valgrind --tool=massif --stacks=yes --massif-out-file=massif.out build/walls-of-doom/walls-of-doom
+#
+cd build/walls-of-doom/
+valgrind --tool=massif --stacks=yes --massif-out-file=massif.out ./walls-of-doom
 # Could use ms_print instead, but massif-visualizer is more useful.
 massif-visualizer massif.out
 rm massif.out
+cd ../../
--- scripts/valgrind/memcheck.sh
+++ scripts/valgrind/memcheck.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 #
 # This is a single-command script to run Memcheck.
-valgrind --leak-check=full --track-origins=yes build/walls-of-doom/walls-of-doom
+#
+cd build/walls-of-doom/
+valgrind --leak-check=full --track-origins=yes ./walls-of-doom
+cd ../../
