c00679849f7f2ff2fd282a8a3554486c1deff1f1
Bernardo Sulzbach <mafagafogigante@gmail.com>
2016-10-27 14:39:12 -0200

    Should make parsed integer a constant

--- walls-of-doom/settings.c
+++ walls-of-doom/settings.c
@@ -72,7 +72,7 @@ static int parse_line(const char **input, char *key, char *value) {
 
 static long parse_value(const char *value, const long minimum,
                         const long maximum, const long fallback) {
-  long integer = strtol(value, NULL, 10);
+  const long integer = strtol(value, NULL, 10);
   if (errno) {
     log_message("Failed to read integer from input string!");
     errno = 0;
