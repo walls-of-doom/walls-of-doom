e609474eaa8c1e45fbbc6638e03012c97327a198
Bernardo Sulzbach <mafagafogigante@gmail.com>
2016-10-29 13:26:14 -0200

    Should use main() as suggested in the SDL website

--- walls-of-doom/main.c
+++ walls-of-doom/main.c
@@ -1,14 +1,23 @@
 #include "io.h"
+#include "logger.h"
 #include "menu.h"
 #include "random.h"
 #include <SDL.h>
 
-int main(void) {
+/* Must be declared with parameters because of SDL 2. */
+int main(int argc, char *argv[]) {
+  int i;
   int result;
   SDL_Window *window;
   SDL_Renderer *renderer;
   seed_random();
   initialize(&window, &renderer);
+  if (argc > 1) {
+    log_message("Ignoring the following command line arguments:");
+    for (i = 1; i < argc; i++) {
+      log_message(argv[i]);
+    }
+  }
   result = main_menu(renderer);
   finalize(&window, &renderer);
   return result;
