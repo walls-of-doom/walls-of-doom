473f63823fe7d8964756356f288ec3b6fcaacef2
Bernardo Sulzbach <mafagafogigante@gmail.com>
2016-11-02 14:39:09 -0200

    Launcher should not print the directory

--- launcher/start-walls-of-doom.sh
+++ launcher/start-walls-of-doom.sh
@@ -3,6 +3,6 @@
 # This script provides a platform-independent launcher for for macOS and Linux.
 #
 # Change to the script directory.
-cd -P -- "$(dirname -- "$0")" && pwd -P
+cd -P -- "$(dirname -- "$0")"
 # Execute the file.
 ./walls-of-doom
