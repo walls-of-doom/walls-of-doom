a38e4740157ca2cfe71db2f6297ff6ccc889f228
Bernardo Sulzbach <mafagafogigante@gmail.com>
2016-11-05 14:32:04 -0200

    Should support the --version command line option
    Closes #140

--- walls-of-doom/main.c
+++ walls-of-doom/main.c
@@ -1,23 +1,58 @@
 #include "io.h"
 #include "logger.h"
+#include "memory.h"
 #include "menu.h"
 #include "random.h"
+#include "text.h"
+#include "version.h"
 #include <SDL.h>
+#include <stdio.h>
+
+typedef enum ParserResult {
+  PARSER_RESULT_CONTINUE,
+  PARSER_RESULT_QUIT
+} ParserResult;
+
+void log_unrecognized_argument(const char *argument) {
+  const size_t start_size = 128;
+  const size_t input_size = strlen(argument);
+  const size_t string_size = start_size + input_size + 1;
+  char *string = NULL;
+  string = resize_memory(string, string_size);
+  sprintf(string, "Unrecognized argument: %s", argument);
+  log_message(string);
+  resize_memory(string, 0);
+}
+
+ParserResult parse_argument(const char *argument) {
+  if (strcmp(argument, "--version") == 0) {
+    printf("%s\n", WALLS_OF_DOOM_VERSION);
+    return PARSER_RESULT_QUIT;
+  } else {
+    log_unrecognized_argument(argument);
+    return PARSER_RESULT_QUIT;
+  }
+}
 
 /* Must be declared with parameters because of SDL 2. */
 int main(int argc, char *argv[]) {
   int i;
-  int result;
+  int quit = 0;
+  int result = 0;
   SDL_Window *window;
   SDL_Renderer *renderer;
-  seed_random();
-  initialize(&window, &renderer);
   if (argc > 1) {
-    log_message("Ignoring the following command line arguments:");
-    for (i = 1; i < argc; i++) {
-      log_message(argv[i]);
+    for (i = 1; i < argc && !quit; i++) {
+      if (parse_argument(argv[i]) == PARSER_RESULT_QUIT) {
+        quit = 1;
+      }
     }
   }
+  if (quit) {
+    return result;
+  }
+  seed_random();
+  initialize(&window, &renderer);
   result = main_menu(renderer);
   finalize(&window, &renderer);
   return result;
