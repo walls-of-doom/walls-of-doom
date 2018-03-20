fb7d28c304b118261b31fb503345a35b87f09097
Bernardo Sulzbach <mafagafogigante@gmail.com>
2018-03-20 19:51:10 -0300

    Should not update and upgrade on Travis

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # This is the script used to setup the environment for Semaphore CI.
-sudo apt-get update
-sudo apt-get upgrade
+# sudo apt-get update
+# sudo apt-get upgrade
 sudo apt-get install -y libegl1-mesa-dev libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev
