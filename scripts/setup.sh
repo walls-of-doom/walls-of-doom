94b0235f7aa59fabd0f7161c3f04f2daf48a66ee
Bernardo Sulzbach <mafagafogigante@gmail.com>
2018-03-20 19:31:46 -0300

    Better scripts for CI

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,10 +4,3 @@
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install -y libegl1-mesa-dev libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev
-mkdir build
-cd build
-cmake ..
-make
-
-# Go back to the project root.
-cd ..
--- scripts/test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# This is the script used to run the tests in Semaphore CI.
-cd build
-make
-./tests
-cd ..
